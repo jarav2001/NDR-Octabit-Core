48dd5b6c392a1eeef6ca02e913495f42c44fe5ab
Jaime A Ramirez A <61329411+jarav2001@users.noreply.github.com>
2026-05-11 21:15:40 -0400

    Create ndr_kernel_interface.c
    Arquitectura de la Interfaz: Explicación de cómo el módulo NDR intercepta las llamadas del Kernel (System Calls) para optimizar el planificador de procesos (Scheduler).

---
+++ ndr_kernel_interface.c
@@ -0,0 +1,6 @@
+/*
+ * NDR-Base8: Kernel-Level Optimization Module
+ * Part of the NDR-Octabit-Core Project
+ * Verified Author: Jaime Andrés Ramírez Arcila (C.I. 15.757.447)
+ * DOI: 10.5281/zenodo.20128879
+ */
